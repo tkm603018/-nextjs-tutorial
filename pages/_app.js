1732868661ea7937f810adea2a6a4e975e2bcabd
“tkm603018” <tkm603018@gmail.com>
2021-09-10 01:43:16 +0900

    共通ヘッダー修正

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,48 @@
+import React from 'react'
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import {CssBaseline} from '@material-ui/core'
+import NewAppBar from '../components/newAppBar'
 import '../styles/globals.css'
+
+
+export default function App({ Component, pageProps }) {
+
+  // React.useEffect(() => {
+  // const jssStyles = document.querySelector('#jss-server-side');
+  // if (jssStyles) {
+  //   jssStyles.parentElement?.removeChild(jssStyles);
+  // }
+  // }, []);
+  
+  const [themeStatus, setThemeStatus] = React.useState(true)
+  console.log("themeStatus", themeStatus)
+
+  const muiTheme = createMuiTheme({
+    palette: {
+      type: themeStatus === true ? 'dark': 'light',
+    },
+  });
+
+  const handleThemeChange = (e) => {
+    setThemeStatus(e.target.checked)
+  }
+
+  
+  return (
+    <div>
+      <ThemeProvider theme={muiTheme} className="Header-main">
+        <CssBaseline />
+        <NewAppBar
+          themeStatus={themeStatus}
+          handleThemeChange={handleThemeChange}
+        >
+          <Component {...pageProps} />
+        </NewAppBar>
+      </ThemeProvider>
+    </div>
+  )
+}
+
 
 // function MyApp({ Component, pageProps }) {
 //   return <Component {...pageProps} />
@@ -6,6 +50,9 @@ import '../styles/globals.css'
 
 // export default MyApp
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+// const theme = createTheme({
+//   status: {
+//     danger: orange[500],
+//   },
+// });
+
